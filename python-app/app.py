08734bfb174cec83b8fa42d1635116922ac52b45
Cesar Joaquim Neto <cesarjn@localhost.localdomain>
2020-09-15 18:51:19 -0300

    included also the secret part 2

--- python-app/app.py
+++ python-app/app.py
@@ -6,7 +6,7 @@ app.config.from_envvar("SETTINGS")
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world", "Message from OpenShift":app.config.MSG}, "Settings":app.config.DB)
+    return json({"hello": "world", "Message from OpenShift":app.config.MSG, "Settings":app.config.DB})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
