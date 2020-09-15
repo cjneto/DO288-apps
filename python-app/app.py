5098c57ff0fd063ab087d3f268fb89140e73cacb
Cesar Joaquim Neto <cesarjn@localhost.localdomain>
2020-09-15 18:46:56 -0300

    included also the secret

--- python-app/app.py
+++ python-app/app.py
@@ -2,10 +2,11 @@ from sanic import Sanic
 from sanic.response import json
 
 app = Sanic("App Name")
+app.config.from_envvar("SETTINGS")
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world", "Message from OpenShift":app.config.MSG})
+    return json({"hello": "world", "Message from OpenShift":app.config.MSG}, "Settings":app.config.DB)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
