1bb62ce479a4f4dfd792700541d46e3674d7aded
Cesar Joaquim Neto <cesarjn@localhost.localdomain>
2020-09-15 16:54:32 -0300

    Receiving a parameter

--- python-app/app.py
+++ python-app/app.py
@@ -5,7 +5,7 @@ app = Sanic("App Name")
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world"})
+    return json({"hello": "world", "Message from OpenShift":app.config.MSG})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
