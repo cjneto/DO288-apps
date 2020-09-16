83227e4f3d98147797b2147f50b005a5672da6af
Cesar Joaquim Neto <cesarjn@localhost.localdomain>
2020-09-16 09:38:08 -0300

    latest version with parameters issues yet

--- python-app/app.py
+++ python-app/app.py
@@ -1,12 +1,19 @@
 from sanic import Sanic
 from sanic.response import json
+import json,os
+
+with open(os.environ['SANIC_ENV']) as jf:
+    cred = json.load(jf)
+
+print(cred)
 
 app = Sanic("App Name")
-#app.config.from_envvar("SANIC_ENV")
+app.config.update(cred)
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world", "Message from OpenShift":app.config.MSG, "Settings":app.config.DB})
+    print(app.config)
+    return json({"hello": "world", "Message from OpenShift":app.config.MSG}) #, "Settings":app.config})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
